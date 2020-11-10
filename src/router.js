94755fd2c8d37d747cc8f27e1fc6492ee33a037c
lukitoharlia760 <70196457+lukitoharlia760@users.noreply.github.com>
2020-11-10 20:01:47 +0700

    first commit

---
+++ src/router.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+function importComponent(path){
+    return () => import(`./components/${path}.vue`)
+}
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name:"admin",
+            component: importComponent('DashboardLayout'),
+            children: [
+                //Dashboard
+                {
+                    path: "/",
+                    name: "Root",
+                    component: importComponent('Dashboard'),
+                },
+                {
+                    path: "/gd",
+                    name:'Guided',
+                    component: importComponent('TodoList/List'),
+                },
+                {
+                    path: "/ugd",
+                    name:'Unguided',
+                    component: importComponent('TodoList/ListUGD'),
+                },
+                {
+                    path: "/tugas",
+                    name:'Tugas',
+                    component: importComponent('TodoList/ListTugas'),
+                },
+            ]
+        },
+    ]
+});
+
+export default router;
